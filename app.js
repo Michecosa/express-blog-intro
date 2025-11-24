78a7afe9441afbb7e68d8770703d3f660d7e7161
Michela <micheladellagatta1@gmail.com>
2025-11-24 12:00:26 +0100

    Retrieve posts in JSON format

--- app.js
+++ app.js
@@ -40,6 +40,12 @@ app.get("/", (req, res) => {
   res.send("Server del mio blog");
 });
 
+app.get("/bacheca", (req, res) => {
+  res.json({
+    posts: posts,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
