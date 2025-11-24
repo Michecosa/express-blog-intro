3e37d4c05763da8f1c71c9de726dc1eec2b245a2
Michela <micheladellagatta1@gmail.com>
2025-11-24 12:08:52 +0100

    Update post images to local paths and simplify JSON response

--- app.js
+++ app.js
@@ -2,36 +2,38 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 /* Array */
 const posts = [
   {
     titolo: "Titolo 1",
     contenuto: "Contenuto 1",
-    immagine: "https://picsum.photos/200",
+    immagine: "/img/ciambellone.jpeg",
     tags: ["tag 1", "tag 2", "tag 3"],
   },
   {
     titolo: "Titolo 2",
     contenuto: "Contenuto 2",
-    immagine: "https://picsum.photos/200",
+    immagine: "/img/cracker_barbabietola.jpeg",
     tags: ["tag 1", "tag 2", "tag 3"],
   },
   {
     titolo: "Titolo 3",
     contenuto: "Contenuto 3",
-    immagine: "https://picsum.photos/200",
+    immagine: "/img/pane_fritto_dolce.jpeg",
     tags: ["tag 1", "tag 2", "tag 3"],
   },
   {
     titolo: "Titolo 4",
     contenuto: "Contenuto 4",
-    immagine: "https://picsum.photos/200",
+    immagine: "/img/pasta_barbabietola.jpeg",
     tags: ["tag 1", "tag 2", "tag 3"],
   },
   {
     titolo: "Titolo 5",
     contenuto: "Contenuto 5",
-    immagine: "https://picsum.photos/200",
+    immagine: "/img/torta_paesana.jpeg",
     tags: ["tag 1", "tag 2", "tag 3"],
   },
 ];
@@ -41,9 +43,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/bacheca", (req, res) => {
-  res.json({
-    posts: posts,
-  });
+  res.json({ posts });
 });
 
 app.listen(port, () => {
