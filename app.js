90e834b0f057e27d178b58b40aa6e97e8db28c29
Michela <micheladellagatta1@gmail.com>
2025-11-24 11:58:32 +0100

    Add array of objects with 5 posts infos

--- app.js
+++ app.js
@@ -2,8 +2,42 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+/* Array */
+const posts = [
+  {
+    titolo: "Titolo 1",
+    contenuto: "Contenuto 1",
+    immagine: "https://picsum.photos/200",
+    tags: ["tag 1", "tag 2", "tag 3"],
+  },
+  {
+    titolo: "Titolo 2",
+    contenuto: "Contenuto 2",
+    immagine: "https://picsum.photos/200",
+    tags: ["tag 1", "tag 2", "tag 3"],
+  },
+  {
+    titolo: "Titolo 3",
+    contenuto: "Contenuto 3",
+    immagine: "https://picsum.photos/200",
+    tags: ["tag 1", "tag 2", "tag 3"],
+  },
+  {
+    titolo: "Titolo 4",
+    contenuto: "Contenuto 4",
+    immagine: "https://picsum.photos/200",
+    tags: ["tag 1", "tag 2", "tag 3"],
+  },
+  {
+    titolo: "Titolo 5",
+    contenuto: "Contenuto 5",
+    immagine: "https://picsum.photos/200",
+    tags: ["tag 1", "tag 2", "tag 3"],
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server del mio blog");
 });
 
 app.listen(port, () => {
